7d34898d079af02b63ad21fdb3f21aa4603b5bf4
sandhata <sandhata@STLINDADM002>
2023-03-05 11:06:10 +0530

    Add project files.

---
+++ XMLFileHandlings/Employees.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLFileHandlings
+{
+
+
+    [XmlRoot(ElementName ="employees")]
+    public class Employees
+    {
+        [XmlElement(ElementName = "employee")]
+        public List<Employee> Employee { get; set; }
+
+    }
+
+    [XmlRoot("employee")]
+    public class Employee
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "position")]
+        public string Position { get; set; }
+        [XmlElement(ElementName = "joinyear")]
+        public string Joinyear { get; set; }
+
+        [XmlElement(ElementName = "salary")]
+        public double Salary { get; set; }
+
+        [XmlAttribute(AttributeName = "category")]
+        public string Category { get; set; }
+
+    }
+
+    
+    public class IDS
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+
+    }
+}
---
+++ XMLFileHandlings/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLFileHandlings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //To sote the FillePath
+                string filePath = "C:\\Users\\sandhata\\source\\repos\\XMLFileHandlings\\XMLFileHandlings\\Employees.xml";
+                //Callling The Method with FilePath Parameter
+                DeserializeTheXmlToObject(filePath);
+            }
+            catch(FileNotFoundException Fe) {
+                Console.WriteLine(Fe.Message);
+            }
+
+        }
+        public static void DeserializeTheXmlToObject(string filePath)
+        {
+            try
+            {
+                //To do Desirialize we must instantiate Xmlserilizer Object with the type that we want to desirialize
+                XmlSerializer xmlSerilaizer = new XmlSerializer(typeof(Employees));
+
+                //Instantiate the StreamReader for reading the Xml file by passing the filePath as arugument for the StreamReader constructor
+                var reader = new StreamReader(filePath);
+
+                //Then Deserialize the file to Object by using Deserialize() present in the XmlSerializer Class
+                //And convert the XmlSerializer type to Employee Type Reading the Value in Console
+                Employees employees = (Employees)xmlSerilaizer.Deserialize(reader);
+
+                //To print values that are present in the Xml file into The Console
+                foreach (Employee employee in employees.Employee)
+                {
+                    Console.WriteLine($"The Name :{employee.Name}," +
+                        $"\nThe Position :{employee.Position}" +
+                        $"\nThe Salary :{employee.Salary}" +
+                        $"\nJoinging year :{employee.Joinyear}" +
+                        $"\nDepartment is :{employee.Category}" + "\n");
+                }
+                Console.WriteLine("De-Serializatio completed successfuly completed");
+            } 
+            catch(IndexOutOfRangeException ie) {
+                Console.WriteLine(ie.Message);
+            }
+            catch(XmlException xe)
+            {
+                Console.WriteLine(xe.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for Working with us");
+            }
+        }
+        
+    }
+
+}
